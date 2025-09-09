87c8f749b2934aae6acad08bd77f3e9988b33bf0
Víctor García <gc.victor@gmail.com>
2025-09-09 15:26:41 +0200

    feat(examples): add examples workspace
    - Add SXO-based examples app for demos
    - Include fonts, pages, and styles for examples

---
+++ examples/sxo.config.js
@@ -0,0 +1,11 @@
+export default {
+    open: false,
+    port: 3000,
+    loaders: {
+        ".svg": "text",
+        ".ttf": "file",
+        ".woff": "file",
+        ".woff2": "file"
+    },
+    publicPath: "./",
+}
