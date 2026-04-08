a4e37b15b7254032e42da903f110502e9b849f0b
Víctor García <gc.victor@gmail.com>
2026-04-08 15:27:24 +0200

    build(examples): upgrade examples app dependencies
    - Upgrade SXO from 0.6.1 to 0.10.1 (breaking changes in config)
    - Upgrade Tailwind CSS from ^4.1.16 to ^4.2.2
    - Upgrade TypeScript from ^5.9.3 to ^6.0.2
    - Add server script for build + start workflow
    - Update sxo.config.js loader configuration for new version

--- examples/sxo.config.js
+++ examples/sxo.config.js
@@ -3,9 +3,13 @@ export default {
     port: 3000,
     loaders: {
         ".svg": "text",
-        ".ttf": "file",
-        ".woff": "file",
-        ".woff2": "file",
+    },
+    build: {
+        loader: {
+            ".ttf": "file",
+            ".woff": "file",
+            ".woff2": "file",
+        },
     },
     publicPath: "./",
     alias: {
